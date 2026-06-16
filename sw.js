08967616db62f770616cedf0e7ce92182f9c4eb4
jkyyyy666 <2247244052@qq.com>
2026-06-16 11:31:17 +0800

    Fix IPA phonetic font rendering - add Noto Sans for garbled character support

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
-const CACHE_NAME = 'english-app-v3';
+﻿const CACHE_NAME = 'english-app-v4';
 const urlsToCache = [
+  'https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400&display=swap',
+  'https://fonts.gstatic.com/s/notosans/v39/o-0bIpQlx3QUlC5A4PNB6Ryti20_6n1iPHjc5aPdu2ui.woff2',
+  'https://fonts.gstatic.com/s/notosans/v39/o-0bIpQlx3QUlC5A4PNB6Ryti20_6n1iPHjc5aPdu2ui.woff2',
   './',
   './index.html',
   './css/style.css',
